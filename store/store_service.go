9f58f2e34acbfe61ec216307f2d3f0816a5d5c67
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-10-01 20:12:11 +0200

    Add saveUrl and retrieveUrl in store_service with testing in store_service_test

--- store/store_service.go
+++ store/store_service.go
@@ -31,12 +31,28 @@ func InitializeStore() *StorageService {
 		DB:       0,
 	})
 
-	pong, err := redisClient.Ping(ctx).Result()
+	_, err := redisClient.Ping(ctx).Result()
 	if err != nil {
 		panic(fmt.Sprintf("Error init Redis: %v", err))
 	}
 
-	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+	fmt.Printf("\nRedis started successfully")
 	storeService.redisClient = redisClient
 	return storeService
+}
+
+
+func SaveUrl(shortUrl string, originalUrl string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+}
+
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
+	}
+	return result
 }
--- store/store_service_test.go
+++ store/store_service_test.go
@@ -15,3 +15,12 @@ func init() {
 func TestStoreInit(t *testing.T) {
 	assert.True(t, testStoreService.redisClient != nil)
 }
+
+func TestInsertionAndRetrieval(t *testing.T) {
+	initialLink := "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
+	shortURL := "Jsz4k57oAX"
+
+	SaveUrl(shortURL, initialLink)
+
+	assert.Equal(t, initialLink, RetrieveInitialUrl(shortURL))
+}
