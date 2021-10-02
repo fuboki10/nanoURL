28c2a00fffc8061d4767c0d1d764d2ca729753b0
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-10-02 14:28:47 +0200

    create temp redis client

--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v7 v7.4.1 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- service/store/store_service.go
+++ service/store/store_service.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/go-redis/redis/v7"
+	"github.com/go-redis/redis"
 )
 
 // Define the struct wrapper around raw Redis client
 type StorageService struct {
 	redisClient *redis.Client
+	tempClinet map[string]string
 }
+
+var tempRedis map[string]string
 
 // Top level declarations for the storeService and Redis context
 var (
@@ -24,27 +27,37 @@ const CacheDuration = 6 * time.Hour
 // Initializing the store service and return a store pointer 
 func InitializeStore() *StorageService {
 	redisClient := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+		Addr: "localhost:6379",
 	})
 
 	_, err := redisClient.Ping().Result()
 	if err != nil {
-		panic(fmt.Sprintf("Error init Redis: %v", err))
+		// connect to temp clinet
+		tempRedis = make(map[string]string)
+		storeService.tempClinet = tempRedis
+	} else {
+		fmt.Printf("\nRedis started successfully")
+		storeService.redisClient = redisClient
 	}
 
-	fmt.Printf("\nRedis started successfully")
-	storeService.redisClient = redisClient
 	return storeService
 }
 
 
 func SaveUrl(shortUrl string, originalUrl string) {
-	storeService.redisClient.Set(shortUrl, originalUrl, CacheDuration).Err()
+	if storeService.redisClient == nil {
+		storeService.tempClinet[shortUrl] = originalUrl
+	} else {
+		storeService.redisClient.Set(shortUrl, originalUrl, CacheDuration).Err()
+	}
 }
 
 func RetrieveInitialUrl(shortUrl string) string {
-	result, _ := storeService.redisClient.Get(shortUrl).Result()
+	result := ""
+	if storeService.redisClient == nil {
+		result = storeService.tempClinet[shortUrl]
+	} else {
+		result, _ = storeService.redisClient.Get(shortUrl).Result()
+	}
 	return result
 }
